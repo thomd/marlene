452128c1d8e165650add7e52835fe100c15ef4f6
Thomas Duerr <thomduerr@gmail.com>
2010-06-22 23:43:40 +0200

    prepend bookmarklet code on top of original javascript file

--- lib/marlene.rb
+++ lib/marlene.rb
@@ -5,14 +5,15 @@ module Marlene
   ANCHOR_TEXTNODE = "I'm a bookmarklet"
 
   def Marlene.to_bookmarklet(input)
-    input.gsub!(/^ +/, '')           # remove line-leading whitespace
-    input.gsub!(/ +$/, '')           # remove line-ending whitespace
-    input.gsub!(/\t/, ' ')           # replace tabs with spaces
-    input.gsub!(/ +/, ' ')           # replace multiple spaces with a single space
-    input.gsub!(/^ *\/\/.+\n/, '')   # remove comment lines starting with a double slash
-    input.gsub!(/\n/, '')            # remove all newlines
-    input = URI.escape(input, ' ')   # URI escape spaces only
-    'javascript:' + input            # prefix with 'javascript:'
+    js = input.to_s
+    js = js.gsub(/^ +/, '')           # remove line-leading whitespace
+    js = js.gsub(/ +$/, '')           # remove line-ending whitespace
+    js = js.gsub(/\t/, ' ')           # replace tabs with spaces
+    js = js.gsub(/ +/, ' ')           # replace multiple spaces with a single space
+    js = js.gsub(/^ *\/\/.+\n/, '')   # remove comment lines starting with a double slash
+    js = js.gsub(/\n/, '')            # remove all newlines
+    js = URI.escape(js, ' ')          # URI escape spaces only
+    'javascript:' + js                # prefix with 'javascript:'
   end
 
 end
